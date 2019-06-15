a49dc95f3c48e3b1d1de21c111fa5680cb8231a8
Lance Finney <lance.finney@oasisdigital.com>
2019-06-15 15:06:39 -0500

    Test DashboardComponent as though it's a class

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,3 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 
 import { HeroService } from '../hero.service';
@@ -11,24 +8,11 @@ fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let svcSpy: jasmine.SpyObj<HeroService>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     svcSpy = jasmine.createSpyObj<HeroService>(['getHeroes']);
     svcSpy.getHeroes.and.returnValue(of(HEROES));
 
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
-      providers: [
-        DashboardComponent,
-        {
-          provide: HeroService, useValue: svcSpy
-        }
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    });
-  }));
-
-  beforeEach(() => {
-    component = TestBed.get(DashboardComponent);
+    component = new DashboardComponent(svcSpy);
     component.ngOnInit();
   });
 
