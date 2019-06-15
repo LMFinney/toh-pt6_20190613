0c098814dc33fbb69970acda9f582b926458246e
Lance Finney <lance.finney@oasisdigital.com>
2019-06-15 14:44:32 -0500

    Simple changes to make test green

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,29 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
+import { HeroService } from '../hero.service';
 import { DashboardComponent } from './dashboard.component';
 
 fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let svcSpy: jasmine.SpyObj<HeroService>;
 
   beforeEach(async(() => {
+    svcSpy = jasmine.createSpyObj<HeroService>(['getHeroes']);
+    svcSpy.getHeroes.and.returnValue(of([]));
+
     TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
+      declarations: [DashboardComponent],
+      imports: [RouterTestingModule.withRoutes([])],
+      providers: [
+        {
+          provide: HeroService, useValue: svcSpy
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
   }));
