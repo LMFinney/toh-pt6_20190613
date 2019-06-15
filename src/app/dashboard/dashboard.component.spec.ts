828da7ad176916c94e67469236b05271c290a8c5
Lance Finney <lance.finney@oasisdigital.com>
2019-06-15 15:02:06 -0500

    Add meat to the dashboard test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,9 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 
 import { HeroService } from '../hero.service';
+import { HEROES } from '../mock-heroes';
 import { DashboardComponent } from './dashboard.component';
 
 fdescribe('DashboardComponent', () => {
@@ -13,7 +15,7 @@ fdescribe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     svcSpy = jasmine.createSpyObj<HeroService>(['getHeroes']);
-    svcSpy.getHeroes.and.returnValue(of([]));
+    svcSpy.getHeroes.and.returnValue(of(HEROES));
 
     TestBed.configureTestingModule({
       declarations: [DashboardComponent],
@@ -36,5 +38,15 @@ fdescribe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should skip the first hero', () => {
+    expect(component.heroes.length).toBe(4);
+    expect(component.heroes[0]).toBe(HEROES[1]);
+  });
+
+  it('should display 4 links', () => {
+    expect(fixture.nativeElement.querySelectorAll('a').length).toBe(4);
+    expect(fixture.debugElement.queryAll(By.css('a')).length).toBe(4);
   });
 });
